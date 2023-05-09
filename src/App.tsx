01246d555017d557cd3c45a8b5114aa55840e67b
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-05-09 14:29:43 -0300

    search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import ArrowIcon from '/images/icon-arrow.svg';
 
 function App() {
 
   return (
     <main className="min-h-screen bg-black ">
       <header className="font-rubik relative bg-hero-mobile md:bg-hero-desktop h-[280px]   bg-cover bg-center w-full">
+
+        <h1 className="py-6 text-2xl font-medium text-center text-white">IP Address Tracker </h1>
+        <div className="flex justify-center mx-auto max-w-[300px]">
+          <input className="w-full pl-4 rounded-s-lg" type="search" placeholder="Search for any IP address or domain" />
+          <button aria-label="search" className="p-4 text-white bg-black rounded-e-lg"> <img src={ArrowIcon} alt="arrow-icon" /></button>
+        </div>
+
         <div className="grid items-center p-4 bg-white rounded-2xl max-w-[300px] left-0 right-0 mx-auto gap-4 absolute top-1/2 ">
           <div className="text-center">
             <span className="text-xs font-medium uppercase text-dark-gray">ip address</span>
